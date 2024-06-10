fb62d22252d1c49b830b1f1c372ad95cf68c5951
programadorluydi <programadorluydi@gmail.com>
2024-06-10 18:50:15 -0300

    Atualizar o menu.php

--- menu.php
+++ menu.php
@@ -1,14 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Meu Sistema</a>
+  <a class="navbar-brand" href="principal.php"><img src="https://png.pngtree.com/png-vector/20220915/ourmid/pngtree-water-nature-logo-company-png-image_6175947.png" width="80px" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(Página atual)</span></a>
+      <a class="nav-item nav-link active" href="principal.php">Home <span class="sr-only">(Página atual)</span></a>
       <a class="nav-item nav-link" href="clientes.php">Clientes</a>
+      <a class="nav-item nav-link" href="categorias.php">Categorias</a>
+      <a class="nav-item nav-link" href="produtos.php">Produtos</a>
       <a class="nav-item nav-link" href="#">Vendas</a>
-      <a class="nav-item nav-link" href="#">Usuarios</a>
+      <a class="nav-item nav-link" href="usuarios.php">Usuarios</a>
     </div>
   </div>
 
